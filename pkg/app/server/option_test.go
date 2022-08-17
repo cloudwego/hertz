01b06e69eaad5f1268332cdf1089228b7fcb133e
byene0923 <byene0923@gmail.com>
2022-08-17 12:39:35 +0800

    fix:add test

--- pkg/app/server/option_test.go
+++ pkg/app/server/option_test.go
@@ -56,6 +56,7 @@ func TestOptions(t *testing.T) {
 		WithALPN(true),
 		WithTraceLevel(stats.LevelDisabled),
 		WithRegistry(nil, info),
+		WithAutoRender(false),
 	})
 	assert.DeepEqual(t, opt.ReadTimeout, time.Second)
 	assert.DeepEqual(t, opt.IdleTimeout, time.Second)
@@ -79,6 +80,7 @@ func TestOptions(t *testing.T) {
 	assert.DeepEqual(t, opt.TraceLevel, stats.LevelDisabled)
 	assert.DeepEqual(t, opt.RegistryInfo, info)
 	assert.DeepEqual(t, opt.Registry, nil)
+	assert.DeepEqual(t, opt.AutoRender, false)
 }
 
 func TestDefaultOptions(t *testing.T) {
