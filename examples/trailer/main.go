257dc63643b55458ba928e554d1801f7e96164cd
wangzekun.zekin <wangzekun.zekin@bytedance.com>
2023-01-11 11:01:15 +0800

    test: update example

--- examples/trailer/main.go
+++ examples/trailer/main.go
@@ -31,7 +31,7 @@ import (
 func main() {
 	h := server.Default(server.WithHostPorts("127.0.0.1:8080"), server.WithStreamBody(true))
 
-	h.GET("/trailer", handler)
+	h.POST("/trailer", handler)
 
 	go h.Spin()
 	time.Sleep(time.Second)
@@ -39,19 +39,31 @@ func main() {
 	c, _ := client.NewClient(client.WithResponseBodyStream(true))
 	req := &protocol.Request{}
 	resp := &protocol.Response{}
-	req.SetMethod(consts.MethodGet)
+	req.SetMethod(consts.MethodPost)
 	req.SetRequestURI("http://127.0.0.1:8080/trailer")
+
+	bs := bytes.NewReader([]byte("ping"))
+	req.SetBodyStream(bs, -1)
+	req.Header.Trailer.Set("AAB", "hertz")
+	req.Header.Trailer.Set("Hertz", "test")
+
 	err := c.Do(context.Background(), req, resp)
 	if err != nil {
 		return
 	}
-	fmt.Println(string(resp.Body()))
+	fmt.Println("client: ", string(resp.Body()))
 	resp.Header.Trailer.VisitAll(visitSingle)
-	fmt.Println(resp.Header.Trailer.Get("AAA"))
-	fmt.Println(resp.Header.Trailer.Get("Hertz"))
+	fmt.Println("client: ", resp.Header.Trailer.Get("AAA"))
+	fmt.Println("client: ", resp.Header.Trailer.Get("Hertz"))
 }
 
 func handler(ctx context.Context, c *app.RequestContext) {
+	fmt.Printf("server: %q\n", c.Request.Body())
+	c.Request.Header.Trailer.VisitAll(visitSingle)
+	fmt.Println("server: ", c.Request.Header.Trailer.Get("AAB"))
+	fmt.Println("server: ", c.Request.Header.Trailer.Get("Hertz"))
+	fmt.Println()
+
 	bs := bytes.NewReader([]byte("Hello World"))
 	c.SetBodyStream(bs, -1)
 	c.Response.Header.Trailer.Set("AAA", "hertz")
