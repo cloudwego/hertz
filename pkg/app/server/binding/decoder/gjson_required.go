b0802fdd3509cfaef1ade29c5afc917cf46827de
fgy <fanguangyu.fgy1995@bytedance.com>
2023-05-17 21:09:31 +0800

    ci: license

--- pkg/app/server/binding/decoder/gjson_required.go
+++ pkg/app/server/binding/decoder/gjson_required.go
@@ -1,25 +1,26 @@
-//go:build stdjson || !(amd64 && (linux || windows || darwin))
-// +build stdjson !amd64 !linux,!windows,!darwin
+// Copyright 2022 CloudWeGo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
-/*
- * Copyright 2022 CloudWeGo Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//go:build gjson || !(amd64 && (linux || windows || darwin))
+// +build gjson !amd64 !linux,!windows,!darwin
 
 package decoder
 
 import (
+	"github.com/cloudwego/hertz/internal/bytesconv"
+	"github.com/cloudwego/hertz/pkg/common/utils"
 	"strings"
 
 	"github.com/tidwall/gjson"
