ba3499547394c3e0a548babee1910096b25a3ec5
violapioggia <2604296771@qq.com>
2023-07-23 23:25:25 +0800

    (test) add more tests in header_test.go

--- pkg/protocol/multipart_test.go
+++ pkg/protocol/multipart_test.go
@@ -80,6 +80,7 @@ Content-Type: application/json
 	assert.Panic(t, func() {
 		err = WriteMultipartForm(&w, form, "")
 	})
+
 	// call WriteField as twice
 	var body bytes.Buffer
 	mw := multipart.NewWriter(&body)
@@ -89,46 +90,6 @@ Content-Type: application/json
 	err = WriteMultipartForm(&w, form, s)
 	assert.NotNil(t, err)
 
-	// 构造*multipart.Form对象和boundary字符串
-	//tempFile, err := os.Create("file1.txt")
-	//if err != nil {
-	//	t.Fatalf("error creating temp file: %v", err)
-	//}
-	//defer tempFile.Close()
-	f := &multipart.Form{
-		Value: map[string][]string{
-			"key1": {"value1"},
-			"key2": {"value2"},
-		},
-		File: map[string][]*multipart.FileHeader{
-			"file1": {
-				{
-					Filename: "file1.txt",
-					Header:   make(textproto.MIMEHeader),
-				},
-			},
-		},
-	}
-
-	// 调用WriteMultipartForm函数，并传递 ResponseRecorder
-	err = WriteMultipartForm(&w, f, "foo")
-	//if err != nil {
-	//	t.Fatalf("unexpected error: %v", err)
-	//}
-
-	// 检查输出是否符合预期
-	expectedOutput := strings.Replace(`--foo
-Content-Disposition: form-data; name="key1"
-        
-value1
---foo
-Content-Disposition: form-data; name="key2"
-        
-value2
---foo
-        
-`, "\n", "\r\n", -1)
-	assert.DeepEqual(t, expectedOutput, w.String())
 	// normal test
 	err = WriteMultipartForm(&w, form, "foo")
 	if err != nil {
