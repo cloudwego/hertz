f2e61b904c6cd59cb973ad507079c738d994738a
fgy <fanguangyu.fgy1995@bytedance.com>
2023-09-19 12:32:53 +0800

    feat: reflect internal test

--- pkg/app/server/binding/reflect_internal_test.go
+++ pkg/app/server/binding/reflect_internal_test.go
@@ -1,29 +1,29 @@
-/*
- * Copyright 2023 CloudWeGo Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2023 CloudWeGo Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
-package decoder
+package binding
 
 import (
 	"reflect"
 	"testing"
 
+	"github.com/cloudwego/hertz/pkg/app/server/binding/internal/decoder"
 	"github.com/cloudwego/hertz/pkg/common/test/assert"
 )
 
-type foo struct {
+type foo2 struct {
 	F1 string
 }
 
@@ -32,9 +32,9 @@ type fooq struct {
 }
 
 func Test_ReferenceValue(t *testing.T) {
-	foo1 := foo{F1: "f1"}
+	foo1 := foo2{F1: "f1"}
 	foo1Val := reflect.ValueOf(foo1)
-	foo1PointerVal := ReferenceValue(foo1Val, 5)
+	foo1PointerVal := decoder.ReferenceValue(foo1Val, 5)
 	assert.DeepEqual(t, "f1", foo1.F1)
 	assert.DeepEqual(t, "f1", foo1Val.Field(0).Interface().(string))
 	if foo1PointerVal.Kind() != reflect.Ptr {
@@ -42,7 +42,7 @@ func Test_ReferenceValue(t *testing.T) {
 	}
 	assert.DeepEqual(t, "*****decoder.foo", foo1PointerVal.Type().String())
 
-	deFoo1PointerVal := ReferenceValue(foo1PointerVal, -5)
+	deFoo1PointerVal := decoder.ReferenceValue(foo1PointerVal, -5)
 	if deFoo1PointerVal.Kind() == reflect.Ptr {
 		t.Errorf("expect a non-pointer, but get a pointer")
 	}
@@ -52,7 +52,7 @@ func Test_ReferenceValue(t *testing.T) {
 func Test_GetNonNilReferenceValue(t *testing.T) {
 	foo1 := (****foo)(nil)
 	foo1Val := reflect.ValueOf(foo1)
-	foo1ValNonNil, ptrDepth := GetNonNilReferenceValue(foo1Val)
+	foo1ValNonNil, ptrDepth := decoder.GetNonNilReferenceValue(foo1Val)
 	if !foo1ValNonNil.IsValid() {
 		t.Errorf("expect a valid value, but get nil")
 	}
@@ -60,7 +60,7 @@ func Test_GetNonNilReferenceValue(t *testing.T) {
 		t.Errorf("expect can set value, but not")
 	}
 
-	foo1ReferPointer := ReferenceValue(foo1ValNonNil, ptrDepth)
+	foo1ReferPointer := decoder.ReferenceValue(foo1ValNonNil, ptrDepth)
 	if foo1ReferPointer.Kind() != reflect.Ptr {
 		t.Errorf("expect a pointer, but get nil")
 	}
@@ -75,7 +75,7 @@ func Test_GetFieldValue(t *testing.T) {
 	idx := 0
 
 	bar1Val := reflect.ValueOf(bar1)
-	parentFieldVal := GetFieldValue(bar1Val, parentIdx)
+	parentFieldVal := decoder.GetFieldValue(bar1Val, parentIdx)
 	if parentFieldVal.Kind() == reflect.Ptr {
 		t.Errorf("expect a non-pointer, but get a pointer")
 	}
