f9bf02dedbfb008995eb92fe8086c70ce1416c2f
Yi Duan <duanyi.aster@bytedance.com>
2025-02-06 11:16:27 +0800

    feat: update sonic api and feature

--- pkg/common/json/sonic.go
+++ pkg/common/json/sonic.go
@@ -13,9 +13,6 @@
 // limitations under the License.
 //
 
-//go:build (linux || windows || darwin) && amd64 && !stdjson
-// +build linux windows darwin
-// +build amd64
 // +build !stdjson
 
 package json
