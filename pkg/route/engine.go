a40dbde8909a12bd74db3a7f4da96191b23dfb9d
ACaiCat <13110818005@qq.com>
2026-08-26 09:42:32 +0800

    fix(route): only log HTTP2 fallback when http2 client connect

--- pkg/route/engine.go
+++ pkg/route/engine.go
@@ -517,10 +517,12 @@ func (engine *Engine) Serve(c context.Context, conn network.Conn) (err error) {
 	if engine.options.H2C {
 		// protocol sniffer
 		buf, _ := conn.Peek(len(bytestr.StrClientPreface))
-		if bytes.Equal(buf, bytestr.StrClientPreface) && engine.protocolServers[suite.HTTP2] != nil {
-			return engine.protocolServers[suite.HTTP2].Serve(c, conn)
+		if bytes.Equal(buf, bytestr.StrClientPreface) {
+			if engine.protocolServers[suite.HTTP2] != nil {
+				return engine.protocolServers[suite.HTTP2].Serve(c, conn)
+			}
+			hlog.SystemLogger().Warn("HTTP2 server is not loaded, request is going to fallback to HTTP1 server")
 		}
-		hlog.SystemLogger().Warn("HTTP2 server is not loaded, request is going to fallback to HTTP1 server")
 	}
 
 	// ALPN path
